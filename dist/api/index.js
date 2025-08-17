e83435f7bfb4b3415ef464c0b95336ebba1bfd48
zacode123 <zuliabegum0@gmail.com>
2025-08-17 19:14:55 +0530

    Update index.js

--- dist/api/index.js
+++ dist/api/index.js
@@ -18,8 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/api/cat", async (_req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", async (_req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get("/api/cat", async (_req, res) => {
