2ce2d581499e2143502cdd176b70836db8077c4c
zacode123 <zuliabegum0@gmail.com>
2025-08-17 19:27:14 +0530

    Update index.js

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const axios = require("axios");
-const path = require("path");
-const { z } = require("zod");  
+import express from "express";
+import axios from "axios";
+import { z } from "zod";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
